a0f4e970392a843388534064af8916a18dcddb01
mudit108-code <127120092+mudit108-code@users.noreply.github.com>
2024-01-09 13:07:29 +0530

    Update app.js
    updated version, a class named "fade-in" is added to the links, and the animation duration and delay are set in the CSS file. This way, you have a cleaner separation between JavaScript and CSS, and it's easier to manage animations in your stylesheets.

--- app.js
+++ app.js
@@ -93,26 +93,23 @@ function removeDarkTheme(){
 }
 
 function navSlide() {
-  const burger = document.querySelector(".burger");
-  const nav = document.querySelector(".div-navbar");
-  const navLinks = document.querySelectorAll(".div-navbar a");
-  
-  burger.addEventListener("click", () => {
-      //Toggle Nav
-      nav.classList.toggle("nav-active");
-      
-      //Animate Links
-      navLinks.forEach((link, index) => {
-          if (link.style.animation) {
-              link.style.animation = ""
-          } else {
-              link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
-          }
-      });
-      //Burger Animation
-      burger.classList.toggle("toggle");
-  });
-  
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".div-navbar");
+    const navLinks = document.querySelectorAll(".div-navbar a");
+
+    burger.addEventListener("click", () => {
+        // Toggle Nav
+        nav.classList.toggle("nav-active");
+
+        // Animate Links
+        navLinks.forEach((link, index) => {
+            link.classList.toggle("fade-in", nav.classList.contains("nav-active"));
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+        });
+
+        // Burger Animation
+        burger.classList.toggle("toggle");
+    });
 }
 
 navSlide();
